60edd584bb8943166e91a3cf9f21f350af121b33
YoungkeunAhn <tosan2094@gmail.com>
2022-02-13 20:14:47 +0900

    파일 이름 수정

--- src/features/OrderHistory/OrderHistory.tsx
+++ src/features/OrderHistory/OrderHistory.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import OrderHistorySearchBar from './searchBar/OrderHistorySearchBar'
 import OrderHistoryTable from './table/OrderHistoryTable'
 
-export default function OrderHistoryView() {
+export default function OrderHistory() {
   return (
     <div>
       <OrderHistorySearchBar />
