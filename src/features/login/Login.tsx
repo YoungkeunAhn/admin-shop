17507a02d33dac5d25be29afd842843be8e9644f
Youngkeun <tosan2094@gmail.com>
2022-03-03 18:11:38 +0900

    로그인 페이지

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -8,29 +8,54 @@ import {
   TextField,
   Typography,
 } from '@material-ui/core'
+import axios from 'axios'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
 import useStyles from './styles'
 
 export default function Login() {
   const classes = useStyles()
   const router = useRouter()
 
-  const onLogin = () => {
-    // axios.get('http://pilot.inautopay.com:8070/test')
-    router.push('/dashboard')
+  const [shopId, setShopId] = useState<string>('')
+  const [passwd, setPasswd] = useState<string>('')
+
+  const onChangeShopId = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setShopId(event.target.value)
+  }
+
+  const onChangePasswd = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswd(event.target.value)
+  }
+  
+
+  const onLogin = async() => {
+    try{
+      // const {data} = await axios.get('http://pilot.inautopay.com:8070/test')
+      const {data} = await axios({
+        method: 'post',
+        url:'http://pilot.inautopay.com:8070/test',
+        data: {passwd, shopid: shopId}
+      })
+      console.log(data)
+    }catch(e){
+      console.error(e)
+    }
+    // router.push('/dashboard')
   }
 
   return (
     <Container maxWidth="xs" className={classes.container}>
       <Box className={classes.root}>
         <MainTitle titleSize="h2" />
-        <TextField fullWidth variant="outlined" label="아이디 입력" />
+        <TextField fullWidth variant="outlined" label="아이디 입력" value={shopId} onChange={onChangeShopId}/>
         <TextField
           fullWidth
           variant="outlined"
           label="비밀번호 입력"
           type="password"
+          value={passwd}
+          onChange={onChangePasswd}
         />
         <Button variant="contained" color="primary" onClick={onLogin}>
           로그인
